db9901daaab837f141fcda9c52361161886a2788
valsam2023 <mvsamaniego1@utpl.edu.ec>
2024-01-03 17:03:42 -0500

    Subir cambios

--- ejemplos/Ejemplos/src/ejemplos02/Ejemplo041.java
+++ ejemplos/Ejemplos/src/ejemplos02/Ejemplo041.java
@@ -13,16 +13,30 @@ public class Ejemplo041 {
 
     public static void main(String[] args) {
         // 
-        obtenerMultiplicacion(primerValor, segundoValor); 
-        
+        int[][] primerValor = {{1, 2, 3}, {3, 2, 4}, {2, 6, 2}};
+        int[][] segundoValor = {{1, 2, 3}, {3, 2, 2}, {3, 1, 2}};
+        int[][] arreglotres = new int[3][3];
+
+        for (int i = 1; i < primerValor.length; i++) {
+            for (int j = 1; j < segundoValor.length; i++) {
+                System.out.printf("%d");
+            }
+        }
+
+        for (int i = 1; i < primerValor.length; i++) {
+            for (int j = 1; j < segundoValor.length; i++) {
+                arreglotres[i][j] = obtenerMultiplicacion(primerValor[i][j],
+                        segundoValor[i][j]);
+            }
+        }
+
     }
-        
-    public static void obtenerSuma(int a, int b){
-        int suma;
-        suma = a + b;
-        System.out.printf("El valor de la suma es: %d\n", suma);
-        
+
+    public static int obtenerMultiplicacion(int a, int b) {
+        int operacion;
+        operacion = a * b;
+        return operacion;
+
     }
-    
-    
+
 }
