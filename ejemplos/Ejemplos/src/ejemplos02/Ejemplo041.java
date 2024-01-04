624075c50bfde163edf59238cdf743e5aa6b1513
valsam2023 <mvsamaniego1@utpl.edu.ec>
2024-01-03 23:59:24 -0500

    Subir cambios

--- ejemplos/Ejemplos/src/ejemplos02/Ejemplo041.java
+++ ejemplos/Ejemplos/src/ejemplos02/Ejemplo041.java
@@ -16,20 +16,36 @@ public class Ejemplo041 {
         int[][] primerValor = {{1, 2, 3}, {3, 2, 4}, {2, 6, 2}};
         int[][] segundoValor = {{1, 2, 3}, {3, 2, 2}, {3, 1, 2}};
         int[][] arreglotres = new int[3][3];
+        int respuesta;
 
-        for (int i = 1; i < primerValor.length; i++) {
-            for (int j = 1; j < segundoValor.length; i++) {
-                System.out.printf("%d");
-            }
-        }
-
-        for (int i = 1; i < primerValor.length; i++) {
-            for (int j = 1; j < segundoValor.length; i++) {
-                arreglotres[i][j] = obtenerMultiplicacion(primerValor[i][j],
+        for (int i = 0; i < primerValor.length; i++) {
+            for (int j = 0; j < segundoValor.length; j++) {
+                respuesta = obtenerMultiplicacion(primerValor[i][j],
                         segundoValor[i][j]);
+                arreglotres[i][j] = respuesta;
             }
         }
+        for (int i = 0; i < primerValor.length; i++) {
+            for (int j = 0; j < primerValor[i].length; j++) {
+                System.out.printf("%d\t", arreglotres[i][j]);
 
+            }
+            System.out.println("\n");
+        }
+        System.out.println("\n");
+        for (int i = 0; i < segundoValor.length; i++) {
+            for (int j = 0; j < segundoValor[i].length; j++) {
+                System.out.printf("%d\t", arreglotres[i][j]);
+            }
+            System.out.println("\n");
+        }
+        System.out.println("\n");
+        for (int i = 0; i < arreglotres.length; i++) {
+            for (int j = 0; j < arreglotres[i].length; j++) {
+                System.out.printf("%d\t", arreglotres[i][j]);
+            }
+            System.out.println("\n");
+        }
     }
 
     public static int obtenerMultiplicacion(int a, int b) {
